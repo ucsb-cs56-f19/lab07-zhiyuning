5642733576691f5bc2ddba20505ed1a17e0a6914
Zhiyu Ning <zhiyuning@ucsb.edu>
2019-11-19 00:10:00 -0800

    ZN - correct the worng import in FC and WC, add Feat,prop,put table in results

--- src/main/java/hello/WebController.java
+++ src/main/java/hello/WebController.java
@@ -12,6 +12,7 @@ import java.util.Map;
 import java.util.HashMap;
 
 import com.nimbusds.oauth2.sdk.client.ClientReadRequest;
+import hello.geojson.FeatureCollection;
 
 @Controller
 public class WebController {
